ac3ba55c4d4cb6658ac136dc1e91359f3cd36181
PISIT\pisit <hnjdragontung@gmail.com>
2024-10-20 20:34:46 +0700

    update api multiple

---
+++ src/app/api/multiple/[id]/route.jsx
@@ -0,0 +1,21 @@
+import {PrismaClient, prismaClient} from "@prisma/client"
+const prisma = new PrismaClient();
+export async function GET(request, {params}) {
+    const multipleid = Number(params.id)
+    const findequation = await prisma.multiple.findUnique({
+        where:{
+            id:multipleid
+        }
+    })
+    return Response.json(findequation)
+}
+
+export async function DELETE(request, {params}) {
+    const multipleid = Number(params.id)
+    const deletemultiple = await prisma.multiple.delete({
+        where:{
+            id:multipleid
+        }
+    })
+    
+}
--- src/app/api/multiple/rout.jsx
+++ src/app/api/multiple/rout.jsx
@@ -2,6 +2,21 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function POST(request) {
-  
+    const{point,xvalue,X,Y,xi} = await request.json()
+    const newpost = await prisma.multiple.create({
+        data:{
+            point,
+            xvalue,
+            X,
+            Y,
+            xi
+        }
+    })
+    return Response.json(newpost)
+    
+}
+export async function GET(request) {
+    const multiple = await prisma.multiple.findMany()
+    return Response.json(multiple)
     
 }
